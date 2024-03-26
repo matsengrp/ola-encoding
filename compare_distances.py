bfbb317517e47f7e03d174f4048f15d9f4a1b122
Harry Richman <dhrichman@gmail.com>
2024-03-25 23:59:20 -0700

    implement spr_neighbor function

--- compare_distances.py
+++ compare_distances.py
@@ -1,4 +1,5 @@
 
+import random
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
@@ -16,13 +17,77 @@ from vector_encoding import (
     write_newicks_of_neighborhood
 )
 
+def spr_neighbor(tree):
+    """
+    return a tree which differs from the input tree by one SPR move
+    """
+    new_tree = tree.copy()
+    node_list = list(new_tree.traverse())
+    # choose regraft location on edge above random node
+    regraft_node = random.choice(node_list)
+    # choose prune location on edge above random node
+    prune_node_found = False
+    while not prune_node_found:
+        prune_node = random.choice(node_list)
+        # check that `prune_node` is not adjacent to `regraft_node`
+        if prune_node.up == regraft_node.up:
+            continue
+        if prune_node.up == regraft_node:
+            continue
+        # check that `prune_node` is not an ancestor of `regraft_node`
+        mrca = new_tree.get_common_ancestor(prune_node, regraft_node)
+        if mrca != prune_node:
+            prune_node_found = True
+    # debugging
+    # print("to prune:", prune_node)
+    # print("to regraft:", regraft_node)
+    # add new node on the regraft edge, above `regraft_node`
+    if not regraft_node.is_leaf():
+        regraft_children = [x for x in regraft_node.children] # copy de-referenced list
+        new_parent = Tree()
+        for node in regraft_children:
+            node.detach()
+            new_parent.add_child(node)
+        regraft_node.add_child(new_parent)
+        new_parent = regraft_node
+    else: # `regraft_node` is a leaf node
+        new_parent = regraft_node.up.add_child()
+        regraft_node.detach()
+        new_parent.add_child(regraft_node)
+    # new_parent.name = "r_new"
+    # debugging
+    # print("after step 1:", new_tree.get_ascii())
+    prune_parent = prune_node.up
+    # prune_parent.name = "p_old"
+    # prune subtree below `prune_node`
+    prune_node.detach()
+    # print("after step 2:", new_tree.get_ascii())
+    # prune_parent.delete()
+    if prune_parent.up is not None:
+        prune_parent.delete()
+        # for node in prune_parent.children:
+        #     prune_parent.up.add_child(node)
+        # prune_parent.up.remove_child(prune_parent)
+    else: # `prune_parent` is root node
+        new_tree = prune_parent.children[0]
+    # print("after step 3:", new_tree.get_ascii())
+    # debugging: clear names
+    # new_parent.name = ""
+    new_parent.add_child(prune_node)
+
+    return new_tree
+
 def plot_random_spr_walks(nleaves=30, nsteps=10, nruns=2, output="test.pdf"):
 
     fig, ax = plt.subplots()
 
     for _ in range(nruns):
+        command = (
+            f"random_spr_walk/random_spr_walk -ntax {nleaves} -niterations {nsteps-1} "
+            "-sfreq 1 > temp.log"
+        )
         run(
-            f"random_spr_walk/random_spr_walk -ntax {nleaves} -niterations {nsteps-1} -sfreq 1 > temp.log", 
+            command, 
             shell=True
         )
         remove_line_numbering(file="temp.log")
