d0cc71eb5a72d272acc96a9c4e16a487009c9e11
Harry Richman <dhrichman@gmail.com>
2025-03-12 05:32:38 +0800

    fix setup file; add package __init__

---
+++ ola/__init__.py
@@ -0,0 +1,12 @@
+from .ola_encoding import (
+    to_vector,
+    to_tree,
+    ola_distance,
+    random_tree_neighbor,
+    random_vector_neighbor,
+    get_tree_neighborhood,
+)
+from .utils import (
+    get_random_tree,
+    get_random_vector,
+)
--- setup.py
+++ setup.py
@@ -1,25 +1,28 @@
-from setuptools import setup, find_packages
+import setuptools 
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name="ola-encoding",
     version="0.1.0",
-    url="https://github.com/matsengrp/ola-encoding.git",
-    author="Matsen Group",
-    author_email="ematsen@gmail.com",
+    url="https://github.com/matsengrp/ola-encoding",
+    author="Harry Richman",
+    author_email="dhrichman@gmail.com",
     description="OLA encoding for phylogenetic trees",
-    long_description=open("README.md").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=find_packages(),
-    install_requires=[
-        "ete3 >= 3.0.0",
-        "pytest >= 7.3",
-    ],
-    python_requires="==3.9.*",
+    packages=["ola"],
     classifiers=[
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.9",
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 3.9",
+    ],
+    python_requires=">=3.9",
+    install_requires=[
+        "ete3 >= 3.0.0",
+        "pytest >= 7.3",
     ],
 )
